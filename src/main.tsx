24cb2e01802df8394903f38febc35e648268112c
kunish <kunish.butt@gmail.com>
2023-03-03 17:51:22 +0800

    feat: add graphiql playground

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { GraphiQL } from "graphiql";
+import "graphiql/graphiql.css";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import App from "~/App";
 import { DEFAULT_ENDPOINT_URL } from "~/constants";
 import "~/index.css";
 import { endpointURL } from "~/store";
-
-const client = new QueryClient();
 
 const getEndpointURL = () => {
   const { searchParams } = new URL(location.href);
@@ -16,10 +18,20 @@ const getEndpointURL = () => {
 
 endpointURL.set(getEndpointURL());
 
+const client = new QueryClient();
+const fetcher = createGraphiQLFetcher({
+  url: endpointURL.get(),
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={client}>
     <ChakraProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route index path="/" element={<App />} />
+          <Route path="/graphql" element={<GraphiQL fetcher={fetcher} />} />
+        </Routes>
+      </BrowserRouter>
     </ChakraProvider>
   </QueryClientProvider>
 );
