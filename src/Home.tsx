002adcc68b1796b5f2b04711821fd4a776cbe167
kunish <kunish.butt@gmail.com>
2023-03-08 22:56:29 +0800

    feat: add SortableGrid

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,76 +4,26 @@ import {
   AccordionIcon,
   AccordionItem,
   AccordionPanel,
-  Button,
   Card,
   CardBody,
-  CardHeader,
   Center,
   Flex,
   Grid,
   Heading,
-  IconButton,
   Spinner,
-  Tooltip,
 } from "@chakra-ui/react";
-import { closestCenter, DndContext, UniqueIdentifier } from "@dnd-kit/core";
-import { restrictToParentElement } from "@dnd-kit/modifiers";
-import { arrayMove, rectSortingStrategy, SortableContext, useSortable } from "@dnd-kit/sortable";
-import { CSS } from "@dnd-kit/utilities";
 import { useStore } from "@nanostores/react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
-import { useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { IoMoveSharp } from "react-icons/io5";
 import SimpleBar from "simplebar-react";
 
 import { gqlClient } from "~/api";
 import { graphql } from "~/gql";
 
-import WithConfirmRemoveButton from "./components/WithConfirmRemoveButton";
+import SortableGrid from "./components/SortableGrid";
 import { QUERY_KEY_CONFIG, QUERY_KEY_GROUP, QUERY_KEY_NODE } from "./constants";
+import { ConfigsQuery, NodesQuery } from "./gql/graphql";
 import { colsPerRowAtom } from "./store";
-
-const DraggableCard = <T extends { id: UniqueIdentifier } & Record<string, unknown>>({
-  data,
-  onSelect,
-  onRemove,
-}: {
-  data: T;
-  onSelect?: () => void;
-  onRemove: () => void;
-}) => {
-  const { t } = useTranslation();
-  const { listeners, attributes, setNodeRef, isDragging, transform, transition } = useSortable({
-    id: data.id,
-  });
-
-  return (
-    <Card
-      ref={setNodeRef}
-      bg={data.selected ? "Highlight" : ""}
-      size="sm"
-      style={{
-        zIndex: isDragging ? Number.MAX_SAFE_INTEGER : 0,
-        transform: CSS.Translate.toString(transform),
-        transition,
-      }}
-    >
-      <CardHeader as={Flex} gap={2} alignItems="center">
-        <IconButton aria-label={t("select")} icon={<IoMoveSharp />} {...listeners} {...attributes} />
-
-        <Tooltip hasArrow label={data.id} placement="top">
-          <Button size="sm" flex={1} noOfLines={1} onClick={() => onSelect && onSelect()}>
-            {data.id}
-          </Button>
-        </Tooltip>
-
-        <WithConfirmRemoveButton aria-label={t("remove")} onRemove={onRemove} />
-      </CardHeader>
-      <CardBody>{JSON.stringify(data)}</CardBody>
-    </Card>
-  );
-};
 
 export default () => {
   const { t } = useTranslation();
@@ -105,24 +55,6 @@ export default () => {
     )
   );
 
-  const [sortableConfigKeys, setSortableConfigKeys] = useState<UniqueIdentifier[]>([]);
-
-  useEffect(() => {
-    if (configQuery.data) {
-      if (sortableConfigKeys.length === 0) {
-        setSortableConfigKeys(configQuery.data.configs.map(({ id }) => id));
-      }
-    }
-  }, [configQuery.data]);
-
-  const sortedConfigItems = useMemo(() => {
-    if (!configQuery.data) {
-      return [];
-    }
-
-    return sortableConfigKeys.map((id) => configQuery.data.configs.find((config) => id === config.id));
-  }, [sortableConfigKeys, configQuery.data]);
-
   const nodeQuery = useQuery(QUERY_KEY_NODE, async () =>
     gqlClient.request(
       graphql(`
@@ -141,24 +73,6 @@ export default () => {
       `)
     )
   );
-
-  const [sortableNodeKeys, setSortableNodeKeys] = useState<UniqueIdentifier[]>([]);
-
-  useEffect(() => {
-    if (nodeQuery.data) {
-      if (sortableConfigKeys.length === 0) {
-        setSortableNodeKeys(nodeQuery.data.nodes.edges.map(({ id }) => id));
-      }
-    }
-  }, [nodeQuery.data]);
-
-  const sortedNodeItems = useMemo(() => {
-    if (!nodeQuery.data) {
-      return [];
-    }
-
-    return sortableNodeKeys.map((id) => nodeQuery.data.nodes.edges.find((config) => id === config.id));
-  }, [sortableNodeKeys, nodeQuery.data]);
 
   const groupQuery = useQuery(QUERY_KEY_GROUP, async () =>
     gqlClient.request(
@@ -250,40 +164,13 @@ export default () => {
             </AccordionButton>
 
             <AccordionPanel>
-              <Flex direction="column" gap={4}>
-                {nodeQuery.isLoading ? (
-                  <Center>
-                    <Spinner />
-                  </Center>
-                ) : (
-                  <Grid gridTemplateColumns={`repeat(${colsPerRow}, 1fr)`} gap={2}>
-                    <DndContext
-                      modifiers={[restrictToParentElement]}
-                      collisionDetection={closestCenter}
-                      onDragEnd={({ over, active }) =>
-                        over &&
-                        active.id !== over.id &&
-                        setSortableNodeKeys((keys) => arrayMove(keys, keys.indexOf(active.id), keys.indexOf(over.id)))
-                      }
-                    >
-                      <SortableContext items={sortableNodeKeys} strategy={rectSortingStrategy}>
-                        {sortedNodeItems.map(
-                          (data) =>
-                            data && (
-                              <DraggableCard
-                                key={data.id}
-                                data={data}
-                                onRemove={() => {
-                                  removeNodeMutation.mutate(data.id);
-                                }}
-                              />
-                            )
-                        )}
-                      </SortableContext>
-                    </DndContext>
-                  </Grid>
-                )}
-              </Flex>
+              <SortableGrid<NodesQuery["nodes"]["edges"]>
+                isLoading={nodeQuery.isLoading}
+                unSortedItems={nodeQuery.data?.nodes.edges}
+                onRemove={(data) => {
+                  removeNodeMutation.mutate(data.id);
+                }}
+              />
             </AccordionPanel>
           </AccordionItem>
 
@@ -297,37 +184,16 @@ export default () => {
             </AccordionButton>
 
             <AccordionPanel>
-              {configQuery.isLoading ? (
-                <Center>
-                  <Spinner />
-                </Center>
-              ) : (
-                <Grid gridTemplateColumns={`repeat(${colsPerRow}, 1fr)`} gap={2}>
-                  <DndContext
-                    modifiers={[restrictToParentElement]}
-                    collisionDetection={closestCenter}
-                    onDragEnd={({ over, active }) =>
-                      over &&
-                      active.id !== over.id &&
-                      setSortableConfigKeys((keys) => arrayMove(keys, keys.indexOf(active.id), keys.indexOf(over.id)))
-                    }
-                  >
-                    <SortableContext items={sortableConfigKeys} strategy={rectSortingStrategy}>
-                      {sortedConfigItems.map(
-                        (data) =>
-                          data && (
-                            <DraggableCard
-                              key={data.id}
-                              data={data}
-                              onSelect={() => selectConfigMutation.mutate(data.id)}
-                              onRemove={() => removeConfigMutation.mutate(data.id)}
-                            />
-                          )
-                      )}
-                    </SortableContext>
-                  </DndContext>
-                </Grid>
-              )}
+              <SortableGrid<ConfigsQuery["configs"]>
+                isLoading={configQuery.isLoading}
+                unSortedItems={configQuery.data?.configs}
+                onSelect={(data) => {
+                  selectConfigMutation.mutate(data.id);
+                }}
+                onRemove={(data) => {
+                  removeConfigMutation.mutate(data.id);
+                }}
+              />
             </AccordionPanel>
           </AccordionItem>
 
---
+++ src/components/SortableGrid.tsx
@@ -0,0 +1,115 @@
+import { Button, Card, CardBody, CardHeader, Center, Flex, Grid, IconButton, Spinner, Tooltip } from "@chakra-ui/react";
+import { closestCenter, DndContext, UniqueIdentifier } from "@dnd-kit/core";
+import { restrictToParentElement } from "@dnd-kit/modifiers";
+import { arrayMove, rectSortingStrategy, SortableContext, useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+import { useStore } from "@nanostores/react";
+import { useEffect, useMemo, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { IoMoveSharp } from "react-icons/io5";
+
+import { colsPerRowAtom } from "~/store";
+
+import WithConfirmRemoveButton from "./WithConfirmRemoveButton";
+
+type List = Array<{ id: UniqueIdentifier } & Record<string, unknown>>;
+
+const DraggableCard = <T extends List[number]>({
+  data,
+  onSelect,
+  onRemove,
+}: {
+  data: T;
+  onSelect?: (data: T) => void;
+  onRemove: (data: T) => void;
+}) => {
+  const { t } = useTranslation();
+  const { listeners, attributes, setNodeRef, isDragging, transform, transition } = useSortable({
+    id: data.id,
+  });
+
+  return (
+    <Card
+      ref={setNodeRef}
+      bg={data.selected ? "Highlight" : ""}
+      size="sm"
+      style={{
+        zIndex: isDragging ? Number.MAX_SAFE_INTEGER : 0,
+        transform: CSS.Translate.toString(transform),
+        transition,
+      }}
+    >
+      <CardHeader as={Flex} gap={2} alignItems="center">
+        <IconButton aria-label={t("select")} icon={<IoMoveSharp />} {...listeners} {...attributes} />
+
+        <Tooltip hasArrow label={data.id} placement="top">
+          <Button size="sm" flex={1} noOfLines={1} onClick={() => onSelect && onSelect(data)}>
+            {data.id}
+          </Button>
+        </Tooltip>
+
+        <WithConfirmRemoveButton aria-label={t("remove")} onRemove={() => onRemove(data)} />
+      </CardHeader>
+      <CardBody>{JSON.stringify(data)}</CardBody>
+    </Card>
+  );
+};
+
+export default <T extends List>({
+  isLoading,
+  unSortedItems,
+  onSelect,
+  onRemove,
+}: {
+  isLoading: boolean;
+  unSortedItems?: T;
+  onSelect?: (data: T[number]) => void;
+  onRemove: (data: T[number]) => void;
+}) => {
+  const colsPerRow = useStore(colsPerRowAtom);
+  const [sortableKeys, setSortableKeys] = useState<UniqueIdentifier[]>([]);
+
+  useEffect(() => {
+    if (unSortedItems) {
+      if (sortableKeys.length === 0) {
+        setSortableKeys(unSortedItems.map(({ id }) => id));
+      }
+    }
+  }, [unSortedItems]);
+
+  const sortedItems = useMemo(() => {
+    if (!unSortedItems) {
+      return [];
+    }
+
+    return sortableKeys.map((id) => unSortedItems.find((config) => id === config.id));
+  }, [sortableKeys, unSortedItems]);
+
+  return (
+    <Flex direction="column" gap={4}>
+      {isLoading ? (
+        <Center>
+          <Spinner />
+        </Center>
+      ) : (
+        <Grid gridTemplateColumns={`repeat(${colsPerRow}, 1fr)`} gap={2}>
+          <DndContext
+            modifiers={[restrictToParentElement]}
+            collisionDetection={closestCenter}
+            onDragEnd={({ over, active }) =>
+              over &&
+              active.id !== over.id &&
+              setSortableKeys((keys) => arrayMove(keys, keys.indexOf(active.id), keys.indexOf(over.id)))
+            }
+          >
+            <SortableContext items={sortableKeys} strategy={rectSortingStrategy}>
+              {sortedItems.map(
+                (item) => item && <DraggableCard key={item.id} data={item} onSelect={onSelect} onRemove={onRemove} />
+              )}
+            </SortableContext>
+          </DndContext>
+        </Grid>
+      )}
+    </Flex>
+  );
+};
