9a77e6246eb8bcf445282fac69618d1b53aa23d2
kunish <kunish.butt@gmail.com>
2023-03-10 18:05:11 +0800

    chore: shared theme

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,11 +1,3 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
-
-export const theme = extendTheme({
-  initialColorMode: "system",
-  useSystemColorMode: true,
-  fonts: { heading: `'Open Sans', sans-serif`, body: `'Raleway', sans-serif` },
-} as ThemeConfig);
-
 export * from "./src/CreateFormDrawer";
 export * from "./src/DescriptiveText";
 export * from "./src/GlobalToast";
@@ -13,4 +5,5 @@ export * from "./src/GrowableInputList";
 export * from "./src/NumberInput";
 export * from "./src/SimpleDisplay";
 export * from "./src/SortableGrid";
+export * from "./src/theme";
 export * from "./src/WithConfirmRemoveButton";
