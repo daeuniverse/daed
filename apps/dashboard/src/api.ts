fbb64cca53eaf269ff159d0a38ab69df01271920
kunish <kunish.butt@gmail.com>
2023-03-10 21:50:25 +0800

    fix: limit query failed toasts

--- apps/dashboard/src/api.ts
+++ apps/dashboard/src/api.ts
@@ -5,7 +5,14 @@ import { GraphQLClient } from "graphql-request";
 import { endpointURLAtom } from "./store";
 
 const onError = (err: unknown) => {
+  const id = "api-error";
+
+  if (toast.isActive(id)) {
+    return;
+  }
+
   toast({
+    id,
     title: (err as Error).message,
     status: "error",
   });
