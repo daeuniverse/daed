0d1a14605195a3d41519d0226c31881b6820b494
kunish <kunish.butt@gmail.com>
2023-06-10 14:48:25 +0800

    build: set base public path to `./`

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
+  base: './',
   resolve: {
     alias: {
       '~': path.resolve('src'),
