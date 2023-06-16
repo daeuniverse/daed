edec958e3e42585a8e4c5f672b164ea46a0754ba
kunish <kunish.butt@gmail.com>
2023-06-16 22:12:29 +0800

    fix(config): refine default values to align with the doc

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { TFunction } from 'i18next'
 import { z } from 'zod'
 
-import { GlobalInput } from '~/schemas/gql/graphql'
+import { GlobalInput, Policy } from '~/schemas/gql/graphql'
 
 export const DEFAULT_ENDPOINT_URL = `${location.protocol}//${location.hostname}:2023/graphql`
 
@@ -96,9 +96,11 @@ export enum UTLSImitate {
 
 export const DEFAULT_LOG_LEVEL = LogLevel.info
 export const DEFAULT_TPROXY_PORT = 12345
+export const DEFAULT_TPROXY_PORT_PROTECT = true
 export const DEFAULT_ALLOW_INSECURE = true
-export const DEFAULT_CHECK_INTERVAL = '10s'
-export const DEFAULT_CHECK_TOLERANCE = '1000ms'
+export const DEFAULT_CHECK_INTERVAL_SECONDS = 30
+export const DEFAULT_CHECK_TOLERANCE_MS = 0
+export const DEFAULT_SNIFFING_TIMEOUT_MS = 100
 export const DEFAULT_UDP_CHECK_DNS = ['dns.google.com:53', '8.8.8.8', '2001:4860:4860::8888']
 export const DEFAULT_TCP_CHECK_URL = ['http://cp.cloudflare.com', '1.1.1.1', '2606:4700:4700::1111']
 export const DEFAULT_DIAL_MODE = DialMode.domain
@@ -106,6 +108,7 @@ export const DEFAULT_TCP_CHECK_HTTP_METHOD = TcpCheckHttpMethod.HEAD
 export const DEFAULT_DISABLE_WAITING_NETWORK = false
 export const DEFAULT_AUTO_CONFIG_KERNEL_PARAMETER = true
 export const DEFAULT_TLS_IMPLEMENTATION = TLSImplementation.tls
+export const DEFAULT_UTLS_IMITATE = UTLSImitate.chrome_auto
 
 export const DEFAULT_CONFIG_NAME = 'global'
 export const DEFAULT_GROUP_NAME = 'proxy'
@@ -113,15 +116,21 @@ export const DEFAULT_GROUP_NAME = 'proxy'
 export const DEFAULT_CONFIG_WITH_INTERFACE = (interfaces?: string[]): GlobalInput => ({
   logLevel: DEFAULT_LOG_LEVEL,
   tproxyPort: DEFAULT_TPROXY_PORT,
+  tproxyPortProtect: DEFAULT_TPROXY_PORT_PROTECT,
   allowInsecure: DEFAULT_ALLOW_INSECURE,
-  checkInterval: DEFAULT_CHECK_INTERVAL,
-  checkTolerance: DEFAULT_CHECK_TOLERANCE,
+  checkInterval: `${DEFAULT_CHECK_INTERVAL_SECONDS}s`,
+  checkTolerance: `${DEFAULT_CHECK_TOLERANCE_MS}ms`,
+  sniffingTimeout: `${DEFAULT_SNIFFING_TIMEOUT_MS}ms`,
   lanInterface: interfaces ? interfaces : [],
   wanInterface: interfaces ? interfaces : [],
   udpCheckDns: DEFAULT_UDP_CHECK_DNS,
   tcpCheckUrl: DEFAULT_TCP_CHECK_URL,
+  tcpCheckHttpMethod: DEFAULT_TCP_CHECK_HTTP_METHOD,
   dialMode: DEFAULT_DIAL_MODE,
   autoConfigKernelParameter: DEFAULT_AUTO_CONFIG_KERNEL_PARAMETER,
+  tlsImplementation: DEFAULT_TLS_IMPLEMENTATION,
+  utlsImitate: DEFAULT_UTLS_IMITATE,
+  disableWaitingNetwork: DEFAULT_DISABLE_WAITING_NETWORK,
 })
 
 export const GET_LOG_LEVEL_STEPS = (t: TFunction) => [
@@ -131,6 +140,8 @@ export const GET_LOG_LEVEL_STEPS = (t: TFunction) => [
   [t('debug'), LogLevel.debug],
   [t('trace'), LogLevel.trace],
 ]
+
+export const DEFAULT_GROUP_POLICY = Policy.MinMovingAvg
 
 export const DEFAULT_ROUTING = `
   pname(NetworkManager, systemd-resolved) -> direct
