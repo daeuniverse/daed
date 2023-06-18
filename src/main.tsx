b04d820c52310a1cb43342bbe71ad3919d36df7a
kunish <kunish.butt@gmail.com>
2023-06-18 14:18:53 +0800

    fix: parse duration correctly to avoid time unit response inconsistency with request

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
 import '~/index.css'
 
+import dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration'
 import ReactDOM from 'react-dom/client'
 
 import { App } from '~/App'
 import { i18nInit } from '~/i18n'
+
+dayjs.extend(duration)
 
 i18nInit().then(() => {
   ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
