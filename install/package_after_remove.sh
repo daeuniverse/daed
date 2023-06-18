2d7289dbb457e3515a48b6e94576fed333acd8a4
Markson <markson@2333ma.top>
2023-06-18 14:14:13 +0800

    package_after_remove.sh: remove useless code

--- install/package_after_remove.sh
+++ install/package_after_remove.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-if [ "$(systemctl is-active daed)" == 'active' ]; then
-    systemctl stop daed.service
-    echo "stopping daed service because daed has been removed."
-fi
-
 systemctl daemon-reload
