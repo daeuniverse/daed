fc5f134ccce7394d8649c11384bd454ab9f2ae9b
kunish <kunish.butt@gmail.com>
2023-06-29 01:20:28 +0800

    feat(WIP): update trojan form tab

--- src/components/NodeModal.tsx
+++ src/components/NodeModal.tsx
@@ -334,10 +334,8 @@ const SSRForm = () => {
 const TrojanForm = () => {
   const { values, onSubmit, getInputProps, reset } = useForm({
     initialValues: {
-      protocol: 'vmess',
-      type: 'none',
-      tls: 'none',
-      net: 'tcp',
+      method: 'origin',
+      obfs: 'none',
     },
   })
 
@@ -347,122 +345,62 @@ const TrojanForm = () => {
         console.log(values)
       })}
     >
-      <Select
-        data={[
-          { label: 'VMESS', value: 'vmess' },
-          { label: 'VLESS', value: 'vless' },
-        ]}
-        {...getInputProps('protocol')}
-      />
+      <TextInput label="Name" {...getInputProps('name')} />
 
-      <TextInput label="Name" {...getInputProps('ps')} />
-
-      <TextInput label="Host" withAsterisk {...getInputProps('add')} />
+      <TextInput label="Host" withAsterisk {...getInputProps('server')} />
 
       <NumberInput label="Port" withAsterisk min={0} max={65535} {...getInputProps('port')} />
 
-      <TextInput label="ID" withAsterisk {...getInputProps('id')} />
-
-      {values.protocol === 'vmess' && <NumberInput label="AlterID" min={0} max={65535} {...getInputProps('aid')} />}
-
-      {values.protocol === 'vmess' && (
-        <Select
-          label="Security"
-          data={[
-            { label: 'auto', value: 'auto' },
-            { label: 'aes-128-gcm', value: 'aes-128-gcm' },
-            { label: 'chacha20-poly1305', value: 'chacha20-poly1305' },
-            { label: 'none', value: 'none' },
-            { label: 'zero', value: 'zero' },
-          ]}
-          {...getInputProps('scy')}
-        />
-      )}
-
-      {values.type !== 'dtls' && (
-        <Select
-          label="TLS"
-          data={[
-            { label: 'off', value: 'none' },
-            { label: 'tls', value: 'tls' },
-            { label: 'xtls', value: 'xtls' },
-          ]}
-          {...getInputProps('tls')}
-        />
-      )}
-
-      {values.tls !== 'none' && <TextInput label="SNI" {...getInputProps('sni')} />}
-
-      {values.tls === 'xtls' && (
-        <Select
-          label="Flow"
-          data={[
-            { label: 'none', value: 'none' },
-            { label: 'xtls-rprx-origin', value: 'xtls-rprx-origin' },
-            { label: 'xtls-rprx-origin-udp443', value: 'xtls-rprx-origin-udp443' },
-            { label: 'xtls-rprx-vision', value: 'xtls-rprx-vision-udp443' },
-          ]}
-          {...getInputProps('flow')}
-        />
-      )}
-
-      {values.tls !== 'none' && (
-        <Checkbox label="AllowInsecure" {...getInputProps('allowInsecure', { type: 'checkbox' })} />
-      )}
+      <TextInput label="Password" withAsterisk {...getInputProps('password')} />
 
       <Select
-        label="Network"
+        label="Protocol"
         withAsterisk
         data={[
-          { label: 'TCP', value: 'tcp' },
-          { label: 'mKCP', value: 'kcp' },
-          { label: 'WebSocket', value: 'ws' },
-          { label: 'HTTP/2', value: 'h2' },
-          { label: 'gRPC', value: 'grpc' },
+          { label: 'origin', value: 'origin' },
+          { label: 'shadowsocks', value: 'shadowsocks' },
         ]}
-        {...getInputProps('net')}
+        {...getInputProps('method')}
       />
 
-      {values.net === 'tcp' && (
+      {values.method === 'shadowsocks' && (
         <Select
-          label="Type"
+          label="Shadowsocks Cipher"
+          withAsterisk
           data={[
-            { label: 'No obfuscation', value: 'none' },
-            { label: 'Obfuscated as Video Calls (SRTP)', value: 'srtp' },
+            { label: 'aes-128-gcm', value: 'aes-128-gcm' },
+            { label: 'aes-256-gcm', value: 'aes-256-gcm' },
+            { label: 'chacha20-poly1305', value: 'chacha20-poly1305' },
+            { label: 'chacha20-ietf-poly1305', value: 'chacha20-ietf-poly1305' },
           ]}
-          {...getInputProps('type')}
+          {...getInputProps('ssCipher')}
         />
       )}
 
-      {values.net === 'kcp' && (
-        <Select
-          label="Type"
-          data={[
-            { label: 'No obfuscation', value: 'none' },
-            { label: 'Obfuscated as Video Calls (SRTP)', value: 'srtp' },
-            { label: 'Obfuscated as Bittorrent (uTP)', value: 'utp' },
-            { label: 'Obfuscated as Wechat Video Calls', value: 'wechat-video' },
-            { label: 'Obfuscated as DTLS1.2 Packets (forcibly TLS on)', value: 'dtls' },
-            { label: 'Obfuscated as WireGuard Packets', value: 'wireguard' },
-          ]}
-          {...getInputProps('type')}
-        />
+      {values.method === 'shadowsocks' && (
+        <TextInput label="Shadowsocks password" withAsterisk {...getInputProps('ssPassword')} />
       )}
 
-      {(values.net === 'ws' ||
-        values.net === 'h2' ||
-        values.tls === 'tls' ||
-        (values.net === 'tcp' && values.type === 'http')) && <TextInput label="Host" {...getInputProps('host')} />}
+      <Checkbox
+        label="Allow Insecure"
+        disabled={values.method !== 'origin' || values.obfs !== 'none'}
+        {...getInputProps('allowInsecure', { type: 'checkbox' })}
+      />
 
-      {values.tls === 'tls' && <TextInput label="Alpn" {...getInputProps('alpn')} />}
+      <TextInput label="SNI(Peer)" {...getInputProps('peer')} />
 
-      {values.net === 'ws' ||
-        values.net === 'h2' ||
-        (values.net === 'tcp' && values.type === 'http' && <TextInput label="Path" {...getInputProps('path')} />)}
+      <Select
+        label="Obfs"
+        data={[
+          { label: 'No obfuscation', value: 'none' },
+          { label: 'websocket', value: 'websocket' },
+        ]}
+        {...getInputProps('obfs')}
+      />
 
-      {values.net === 'mkcp' || (values.net === 'kcp' && <TextInput label="Seed" {...getInputProps('path')} />)}
+      {values.obfs === 'websocket' && <TextInput label="Websocket Host" {...getInputProps('host')} />}
 
-      {values.net === 'grpc' && <TextInput label="ServiceName" {...getInputProps('path')} />}
+      {values.obfs === 'websocket' && <TextInput label="Websocket Path" {...getInputProps('path')} />}
 
       <FormActions reset={reset} />
     </form>
