8be46fd54f2c0c217e26c6195e5acd171e2c55f5
kunish <kunish.butt@gmail.com>
2023-06-29 01:00:27 +0800

    feat(WIP): update SSR form tab

--- src/components/NodeModal.tsx
+++ src/components/NodeModal.tsx
@@ -10,6 +10,7 @@ const V2rayForm = () => {
       type: 'none',
       tls: 'none',
       net: 'tcp',
+      scy: 'auto',
     },
   })
 
@@ -146,6 +147,7 @@ const SSForm = () => {
   const { values, onSubmit, getInputProps, reset } = useForm({
     initialValues: {
       plugin: '',
+      method: 'aes-128-gcm',
       obfs: '',
     },
   })
@@ -241,10 +243,9 @@ const SSForm = () => {
 const SSRForm = () => {
   const { values, onSubmit, getInputProps, reset } = useForm({
     initialValues: {
-      protocol: 'vmess',
-      type: 'none',
-      tls: 'none',
-      net: 'tcp',
+      method: 'aes-128-cfb',
+      proto: 'origin',
+      obfs: 'plain',
     },
   })
 
@@ -254,122 +255,76 @@ const SSRForm = () => {
         console.log(values)
       })}
     >
-      <Select
-        data={[
-          { label: 'VMESS', value: 'vmess' },
-          { label: 'VLESS', value: 'vless' },
-        ]}
-        {...getInputProps('protocol')}
-      />
+      <TextInput label="Name" withAsterisk {...getInputProps('name')} />
 
-      <TextInput label="Name" {...getInputProps('ps')} />
-
-      <TextInput label="Host" withAsterisk {...getInputProps('add')} />
+      <TextInput label="Host" withAsterisk {...getInputProps('server')} />
 
       <NumberInput label="Port" withAsterisk min={0} max={65535} {...getInputProps('port')} />
 
-      <TextInput label="ID" withAsterisk {...getInputProps('id')} />
-
-      {values.protocol === 'vmess' && <NumberInput label="AlterID" min={0} max={65535} {...getInputProps('aid')} />}
-
-      {values.protocol === 'vmess' && (
-        <Select
-          label="Security"
-          data={[
-            { label: 'auto', value: 'auto' },
-            { label: 'aes-128-gcm', value: 'aes-128-gcm' },
-            { label: 'chacha20-poly1305', value: 'chacha20-poly1305' },
-            { label: 'none', value: 'none' },
-            { label: 'zero', value: 'zero' },
-          ]}
-          {...getInputProps('scy')}
-        />
-      )}
-
-      {values.type !== 'dtls' && (
-        <Select
-          label="TLS"
-          data={[
-            { label: 'off', value: 'none' },
-            { label: 'tls', value: 'tls' },
-            { label: 'xtls', value: 'xtls' },
-          ]}
-          {...getInputProps('tls')}
-        />
-      )}
-
-      {values.tls !== 'none' && <TextInput label="SNI" {...getInputProps('sni')} />}
-
-      {values.tls === 'xtls' && (
-        <Select
-          label="Flow"
-          data={[
-            { label: 'none', value: 'none' },
-            { label: 'xtls-rprx-origin', value: 'xtls-rprx-origin' },
-            { label: 'xtls-rprx-origin-udp443', value: 'xtls-rprx-origin-udp443' },
-            { label: 'xtls-rprx-vision', value: 'xtls-rprx-vision-udp443' },
-          ]}
-          {...getInputProps('flow')}
-        />
-      )}
-
-      {values.tls !== 'none' && (
-        <Checkbox label="AllowInsecure" {...getInputProps('allowInsecure', { type: 'checkbox' })} />
-      )}
+      <TextInput label="Password" withAsterisk {...getInputProps('password')} />
 
       <Select
-        label="Network"
+        label="Method"
         withAsterisk
         data={[
-          { label: 'TCP', value: 'tcp' },
-          { label: 'mKCP', value: 'kcp' },
-          { label: 'WebSocket', value: 'ws' },
-          { label: 'HTTP/2', value: 'h2' },
-          { label: 'gRPC', value: 'grpc' },
+          { label: 'aes-128-cfb', value: 'aes-128-cfb' },
+          { label: 'aes-192-cfb', value: 'aes-192-cfb' },
+          { label: 'aes-256-cfb', value: 'aes-256-cfb' },
+          { label: 'aes-128-ctr', value: 'aes-128-ctr' },
+          { label: 'aes-192-ctr', value: 'aes-192-ctr' },
+          { label: 'aes-256-ctr', value: 'aes-256-ctr' },
+          { label: 'aes-128-ofb', value: 'aes-128-ofb' },
+          { label: 'aes-192-ofb', value: 'aes-192-ofb' },
+          { label: 'aes-256-ofb', value: 'aes-256-ofb' },
+          { label: 'dae-cfb', value: 'dae-cfb' },
+          { label: 'bf-cfb', value: 'bf-cfb' },
+          { label: 'cast5-cfb', value: 'cast5-cfb' },
+          { label: 'rc4-md5', value: 'rc4-md5' },
+          { label: 'chacha20', value: 'chacha20' },
+          { label: 'chacha20-ietf', value: 'chacha20-ietf' },
+          { label: 'salsa20', value: 'salsa20' },
+          { label: 'camellia-128-cfb', value: 'camellia-128-cfb' },
+          { label: 'camellia-192-cfb', value: 'camellia-192-cfb' },
+          { label: 'camellia-256-cfb', value: 'camellia-256-cfb' },
+          { label: 'idea-cfb', value: 'idea-cfb' },
+          { label: 'rc2-cfb', value: 'rc2-cfb' },
+          { label: 'seed-cfb', value: 'seed-cfb' },
+          { label: 'none', value: 'none' },
         ]}
-        {...getInputProps('net')}
+        {...getInputProps('method')}
       />
 
-      {values.net === 'tcp' && (
-        <Select
-          label="Type"
-          data={[
-            { label: 'No obfuscation', value: 'none' },
-            { label: 'Obfuscated as Video Calls (SRTP)', value: 'srtp' },
-          ]}
-          {...getInputProps('type')}
-        />
-      )}
+      <Select
+        label="Protocol"
+        withAsterisk
+        data={[
+          { label: 'origin', value: 'origin' },
+          { label: 'verify_sha1', value: 'verify_sha1' },
+          { label: 'auth_sha1_v4', value: 'auth_sha1_v4' },
+          { label: 'auth_aes128_md5', value: 'auth_aes128_md5' },
+          { label: 'auth_aes128_sha1', value: 'auth_aes128_sha1' },
+          { label: 'auth_chain_a', value: 'auth_chain_a' },
+          { label: 'auth_chain_b', value: 'auth_chain_b' },
+        ]}
+        {...getInputProps('proto')}
+      />
 
-      {values.net === 'kcp' && (
-        <Select
-          label="Type"
-          data={[
-            { label: 'No obfuscation', value: 'none' },
-            { label: 'Obfuscated as Video Calls (SRTP)', value: 'srtp' },
-            { label: 'Obfuscated as Bittorrent (uTP)', value: 'utp' },
-            { label: 'Obfuscated as Wechat Video Calls', value: 'wechat-video' },
-            { label: 'Obfuscated as DTLS1.2 Packets (forcibly TLS on)', value: 'dtls' },
-            { label: 'Obfuscated as WireGuard Packets', value: 'wireguard' },
-          ]}
-          {...getInputProps('type')}
-        />
-      )}
+      {values.proto !== 'origin' && <TextInput label="Protocol Param" {...getInputProps('protoParam')} />}
 
-      {(values.net === 'ws' ||
-        values.net === 'h2' ||
-        values.tls === 'tls' ||
-        (values.net === 'tcp' && values.type === 'http')) && <TextInput label="Host" {...getInputProps('host')} />}
+      <Select
+        label="Obfs"
+        withAsterisk
+        data={[
+          { label: 'plain', value: 'plain' },
+          { label: 'http_simple', value: 'http_simple' },
+          { label: 'http_post', value: 'http_post' },
+          { label: 'random_head', value: 'random_head' },
+          { label: 'tls1.2_ticket_auth', value: 'tls1.2_ticket_auth' },
+        ]}
+        {...getInputProps('obfs')}
+      />
 
-      {values.tls === 'tls' && <TextInput label="Alpn" {...getInputProps('alpn')} />}
-
-      {values.net === 'ws' ||
-        values.net === 'h2' ||
-        (values.net === 'tcp' && values.type === 'http' && <TextInput label="Path" {...getInputProps('path')} />)}
-
-      {values.net === 'mkcp' || (values.net === 'kcp' && <TextInput label="Seed" {...getInputProps('path')} />)}
-
-      {values.net === 'grpc' && <TextInput label="ServiceName" {...getInputProps('path')} />}
+      {values.obfs !== 'plain' && <TextInput label="Obfs Param" {...getInputProps('obfsParam')} />}
 
       <FormActions reset={reset} />
     </form>
