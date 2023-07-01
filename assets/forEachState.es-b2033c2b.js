0da5c397dcf5e49562215e2ddf8e490cb4c69a0c
kunish <kunish@users.noreply.github.com>
2023-07-01 18:45:43 +0000

    deploy: fa55f7c8ab82aa87d67624f507293373f332dc34

---
+++ assets/forEachState.es-b2033c2b.js
@@ -0,0 +1,1 @@
+var a=Object.defineProperty,l=(t,n)=>a(t,"name",{value:n,configurable:!0});function f(t,n){const r=[];let e=t;for(;e!=null&&e.kind;)r.push(e),e=e.prevState;for(let o=r.length-1;o>=0;o--)n(r[o])}l(f,"forEachState");export{f as s};
---
+++ assets/mode-indent.es-057a4f6a.js
@@ -0,0 +1,1 @@
+var o=Object.defineProperty,v=(e,n)=>o(e,"name",{value:n,configurable:!0});function d(e,n){var t,i;const{levels:l,indentLevel:r}=e;return((!l||l.length===0?r:l.at(-1)-(!((t=this.electricInput)===null||t===void 0)&&t.test(n)?1:0))||0)*(((i=this.config)===null||i===void 0?void 0:i.indentUnit)||0)}v(d,"indent");export{d as r};
